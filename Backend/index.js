18a58a5913f3cdf688e2fe1c27af59142168559b
Atanu Pandit <atanupandit623@gmail.com>
2024-12-18 10:34:56 +0530

    production ready

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./utils/db.js";
 dotenv.config({});
 import productRoute from "./routes/product.route.js";
@@ -8,6 +9,8 @@ import productRoute from "./routes/product.route.js";
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+const _dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(urlencoded({ extended: true, limit: "10mb" }));
@@ -19,6 +22,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use("/api/v2/products", productRoute);
+
+app.use(express.static(path.join(_dirname, "/Frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "Frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectDB();
